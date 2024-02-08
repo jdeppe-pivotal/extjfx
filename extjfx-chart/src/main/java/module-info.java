805b2728835fa204fc613325068eb02733a42c46
Jens Deppe <jdeppe@vmware.com>
2024-02-08 08:52:02 -0800

    Convert to Java module

---
+++ extjfx-chart/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module extjfx.chart {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+  exports cern.extjfx.chart;
+  exports cern.extjfx.chart.data;
+  exports cern.extjfx.chart.plugins;
+}
